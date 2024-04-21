506a9c0bc4de11bddeb3ff9978eac5e2e81f8af4
Carlos0550 <carlospelinski05@gmail.com>
2024-04-20 23:11:44 -0300

    CinnaWeather

--- src/components/WeatherComponent.jsx
+++ src/components/WeatherComponent.jsx
@@ -1,23 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Swal from 'sweetalert2'
 import { useUserData } from './context/context';
 import "./css/weather.css";
 
 function WeatherComponent() {
-  const { weather } = useUserData();
-  
+  const { location,setLocation } = useUserData();
+
+  const [locationData, setLocationData] = useState({
+    location: ''
+  })
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setLocationData((prevState)=>({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (locationData.location === "") {
+      Swal.fire({
+        title: "Campos vacios!",
+        text: "El buscador no debe estar vacio!",
+        icon: "warning"
+      });
+    }else{
+      setLocation(locationData.location)
+    }
+  }
+
   return (
     <div>
         <div className='weather__container'>
           <h1 className='weather__title'>Cinnamon Weather</h1>
-          {weather && (
-            <>
-              <p className='weather__items'>{weather.location.name}, {weather.location.region}, {weather.location.country}</p>
-              <p className='weather__items'>Ultima Actualizacion: {weather.current.last_updated}</p>
-              <p className='weather__items'>Humedad: {weather.current.humidity}%</p>
-              <p className='weather__items'>Temperatura: {weather.current.temp_c}°C</p>
-              <p className='weather__items'>Condicion: {weather.current.condition.text}</p>
-            </>
-          )}
+          <form onSubmit={handleSubmit} className="weather-form">
+            <label className="weather-label">
+              Ingresa una ciudad o región para ver el clima
+              <input
+                type="text"
+                value={locationData.location}
+                onChange={handleInputChange}
+                placeholder="Ej. Posadas, Misiones, Argentina"
+                name="location"
+                className="weather-input"
+              />
+            </label>
+            <button type="submit" className="weather-button">
+              Buscar
+            </button>
+          </form>
+            {location && location.location && (
+              <>
+                <p className='weather__items'>{location.location.name}, {location.location.region}, {location.location.country}</p>
+                <p className='weather__items'>Ultima Actualizacion: {location.current.last_updated}</p>
+                <p className='weather__items'>Humedad: {location.current.humidity}%</p>
+                <p className='weather__items'>Temperatura: {location.current.temp_c}°C</p>
+                <p className='weather__items'>Condicion: {location.current.condition.text}</p>
+              </>
+            )}
+
         </div>
     </div>
   );
--- src/components/context/context.js
+++ src/components/context/context.js
@@ -12,46 +12,28 @@ export const useUserData = () => {
 }
 
 export const UserContextProvider = ({children}) => {
-    const [weather, setWeather] = useState(null)
     const [location, setLocation] = useState(null)
     const API_KEY = process.env.REACT_APP_API_KEY;
-  
     useEffect(() => {
-      const getUserLocation = () => {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-            position => {
-              const { latitude, longitude } = position.coords;
-              setLocation({ latitude, longitude });
-            },
-            error => {
-              console.error("Error getting user Location: ", error);
-            }
-          );
-        } else {
-          console.error("Geolocation is not supported by this browser!");
-        }
-      };
-      getUserLocation();
-    }, []);
-  
-    useEffect(() => {
-      
-      
       setTimeout(() => {
         if (location) {
-          fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location.latitude},${location.longitude}&aqi=no`)
+          fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=no`)
             .then(response => response.json())
-            .then(data => setWeather(data))
+            .then(data => {
+              if (data && data.location && data.current) {
+                setLocation(data)
+              }else{
+                console.log("Error obteniendo el clima: ", data)
+              }
+            })
             .catch(error => console.error("Error obteniendo el clima: ", error));
         }
-      }, 1000);
-      
+      }, 100);
     }, [location, API_KEY]);
-
+    console.log(location)
 
     return(
-        <UserContext.Provider value={{weather}}>
+        <UserContext.Provider value={{setLocation, location}}>
             {children}
         </UserContext.Provider>
     )
